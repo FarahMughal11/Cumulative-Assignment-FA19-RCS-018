a2142351acef6f317fa48ed4bb3927f2b2c98cb3
FarahMughal <tw7744@gmail.com>
2020-08-06 06:45:28 +0500

    updated

--- Final Project Assignment/Assets/Scripts/Menu.cs
+++ Final Project Assignment/Assets/Scripts/Menu.cs
@@ -60,8 +60,19 @@ public class Menu : MonoBehaviour
 
     public void Quit()
     {
-        Application.Quit();
+        // Application.Quit();
+        #if (UNITY_EDITOR || DEVELOPMENT_BUILD)
+            Debug.Log(this.name+" : "+this.GetType()+" : "+System.Reflection.MethodBase.GetCurrentMethod().Name); 
+        #endif
+        #if (UNITY_EDITOR)
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif (UNITY_STANDALONE)
+            Application.Quit();
+        #elif (UNITY_WEBGL)
+            Application.OpenURL("about:blank");
+        #endif
     }
+
     //3 buttons of mlagent
     public void Penguingame()
     {
